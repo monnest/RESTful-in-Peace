7f1522a2cb97e2106ab9f257c0a2ac81996f7924
Monir Hossain <monir198@gmail.com>
2017-07-24 21:54:23 +0600

    Added user role domain

---
+++ src/main/java/com/monir/techiehunt/model/UserRole.java
@@ -0,0 +1,50 @@
+package com.monir.techiehunt.model;
+
+
+import javax.persistence.*;
+
+public class UserRole {
+    private Integer userRoleId;
+    private User user;
+    private String role;
+
+
+    public UserRole() {}
+
+    public UserRole(Integer userRoleId, User user, String role) {
+        this.userRoleId = userRoleId;
+        this.user = user;
+        this.role = role;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_role_id",
+            unique = true, nullable = false)
+    public Integer getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Integer userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "username", nullable = false)
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Column(name = "role", nullable = false, length = 45)
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
